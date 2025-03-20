8b2dfd16503aed1c211fa594805008683ba492fb
sebastian <sebastianhsolorzano@gmail.com>
2025-03-20 11:53:04 -0400

    mostly completed isrectangle (has some false positives)

--- Group Project/isRectangle.c
+++ Group Project/isRectangle.c
@@ -8,7 +8,37 @@
 
 bool isRectangle(SIDES q) {
 
-	//two ways to prove if rectangle:
+	int equality = 0;
+	
+	if (q.AB == q.AC || q.AB == q.AD || q.AB == q.BC || q.AB == q.BD || q.AB == q.CD) {
+		equality++;
+	}
 
+	if (q.AC == q.AD || q.AC == q.BC || q.AC == q.BD || q.AC == q.CD) {
+		equality++;
+	}
+
+	if (q.AD == q.BC || q.AD == q.BD || q.AD == q.CD) {
+		equality++;
+	}
+
+	if (q.BC == q.BD || q.BC == q.CD) {
+		equality++;
+	}
+
+	if (q.BD == q.CD) {
+		equality++;
+	}
+	//cant use else because we have to check all of these
+
+	if (equality == 3) {
+		return true;
+	}
+	if(equality == 4){
+		return false;
+	}
+
+	//unfortunately, gives false positive for trapezoids with three equal sides, diamonds with four equal sides,
+	//and gives false negative for squares. have to check for simultaneus equality somehow
 
 }
