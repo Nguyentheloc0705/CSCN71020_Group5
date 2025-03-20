44d3328e28c5f7de1b96bc303ff771fec446ea45
David Eshak <Deshak1811@conestogac.on.ca>
2025-03-20 14:12:12 -0400

    starting unit tests

--- Project Testing/Project Testing.cpp
+++ Project Testing/Project Testing.cpp
@@ -15,18 +15,47 @@ namespace ProjectTesting
 	{
 	public:
 		
-		TEST_METHOD(isTriangle_pass_case)
+		TEST_METHOD(isTriangle_pass_case_base)
 		{
-			float l1 = 15, l2 = 16, l3 =15;
+			float l1 = 3, l2 = 4, l3 = 5;
 			bool result = isTriangle(l1, l2, l3);
 			Assert::AreEqual(true, result);
 		}
 
-		TEST_METHOD(isTriangle_pass_case)
+		TEST_METHOD(isTriangle_pass_case_equilateral)
 		{
-			float l1 = 15, l2 = 16, l3 = 15;
+			float l1 = 5, l2 = 5, l3 = 5;
 			bool result = isTriangle(l1, l2, l3);
 			Assert::AreEqual(true, result);
 		}
+
+		TEST_METHOD(isTriangle_pass_case_isosceles)
+		{
+			float l1 = 5, l2 = 5, l3 = 8;
+			bool result = isTriangle(l1, l2, l3);
+			Assert::AreEqual(true, result);
+		}
+
+		TEST_METHOD(isTriangle_fail_case_123)
+		{
+			float l1 = 1, l2 = 2, l3 = 3;
+			bool result = isTriangle(l1, l2, l3);
+			Assert::AreEqual(false, result);
+		}
+
+		TEST_METHOD(isTriangle_fail_case_0)
+		{
+			float l1 = 0, l2 = 4, l3 = 5;
+			bool result = isTriangle(l1, l2, l3);
+			Assert::AreEqual(false, result);
+		}
+
+		TEST_METHOD(isTriangle_fail_case_Not_Triangle)
+		{
+			float l1 = 10, l2 = 11, l3 = 30;
+			bool result = isTriangle(l1, l2, l3);
+			Assert::AreEqual(false, result);
+		}
+
 	};
 }
