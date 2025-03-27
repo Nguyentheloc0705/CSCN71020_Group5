d31ddef7be473194816ddb7b87b3eabd117699a0
sebastian <sebastianhsolorzano@gmail.com>
2025-03-27 12:14:29 -0400

    solved problems with concave quadrilaterals (now works with all quadrilaterals). unfortunately, had to change some function names.

--- Group Project/sidelengths.h
+++ Group Project/sidelengths.h
@@ -7,25 +7,20 @@ typedef enum sideType {
 	diagonal
 }stypes; //intentionally mispelled so it isnt mistaken for something else
 
-typedef struct sides {
-	float AB;
-	stypes ABtype;
+typedef struct side {
+	double length;
+	stypes type;
+}SIDE;
 
-	float AC;
-	stypes ACtype;
+typedef struct quadrilateral {
+	SIDE AB;
+	SIDE AC;
+	SIDE AD;
+	SIDE BC;
+	SIDE BD;
+	SIDE CD;
+} QUADRI;
 
-	float AD;
-	stypes ADtype;
 
-	float BC;
-	stypes BCtype;
-
-	float BD;
-	stypes BDtype;
-
-	float CD;
-	stypes CDtype;
-	//surprisingly, making a new struct (length, type) would be more annoying than this
-} SIDES;
-
-SIDES getSides(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4);
+SIDE getSideLength(float x1, float y1, float x2, float y2);
+QUADRI getQuadri(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4);
